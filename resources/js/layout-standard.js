9f8dcb0b0db96f53999116d6d45febc28a7138c0
Daniel Wirz <d.wirz@iqual.ch>
2021-07-29 10:12:06 +0000

    Leave filters open after applying

--- resources/js/layout-standard.js
+++ resources/js/layout-standard.js
@@ -1,8 +1,25 @@
 (function ($, Drupal, drupalSettings) {
   Drupal.behaviors.iq_bef_extensions_layout_standard = {
     attach: function (context, settings) {
+
+      if (window.view_filter_active) {
+        Object.keys(window.view_filter_active).forEach(function(key){
+          if (window.view_filter_active[key]) {
+            $('[data-toggable="' + key + '"]').addClass('active');
+          }
+        })
+      } else{
+        window.view_filter_active = {};
+      }
+
       $('[data-toggle]').click(function(){
-        $('[data-toggable="' + $(this).data('toggle') + '"]').toggleClass('active')
+        $('[data-toggable="' + $(this).data('toggle') + '"]').toggleClass('active');
+        if ($('[data-toggable="' + $(this).data('toggle') + '"]').hasClass('active')) {
+          window.view_filter_active[$(this).data('toggle')] = true;
+        }
+        else {
+          delete window.view_filter_active[$(this).data('toggle')];
+        }
       });
 
       $('.sorting-region select').focus(function(){
