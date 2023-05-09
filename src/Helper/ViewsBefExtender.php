9fc5616e6141a2e761cb15d9908494e2c2d14419
Philippe von Bergen <ph.vonbergen@iqual.ch>
2023-05-09 13:12:37 +0000

    Add view options for flter value storage

---
+++ src/Helper/ViewsBefExtender.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Drupal\iq_bef_extensions\Helper;
+
+use Drupal\better_exposed_filters\Plugin\views\exposed_form\BetterExposedFilters;
+
+class ViewsBefExtender extends BetterExposedFilters {
+  use ViewsExposedFormExtenderTrait;
+}
---
+++ src/Helper/ViewsVeflBefExtender.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Drupal\iq_bef_extensions\Helper;
+
+use Drupal\vefl_bef\Plugin\views\exposed_form\VeflBef;
+
+class ViewsVeflBefExtender extends VeflBef {
+  use ViewsExposedFormExtenderTrait;
+}
