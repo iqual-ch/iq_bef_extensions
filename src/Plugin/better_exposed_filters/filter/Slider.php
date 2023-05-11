dfd787804aa1e00f2d9fbb909404abb68a7e8161
Philippe von Bergen <ph.vonbergen@iqual.ch>
2023-05-11 08:05:35 +0000

    Use new getFilterIds on Slider

--- src/Plugin/better_exposed_filters/filter/Slider.php
+++ src/Plugin/better_exposed_filters/filter/Slider.php
@@ -211,10 +211,8 @@ class Slider extends DefaultWidget {
 
     if ($filter->isExposed() && empty($this->view->selective_filter) && empty($form_state->getUserInput()[$this->getExposedFilterFieldId()]['min']) && empty($form_state->getUserInput()[$this->getExposedFilterFieldId()]['max'])) {
 
-      [$table, $column, $referenceColumn] = $this->getTableAndColumn();
       $relationship = ($filter->options['relationship']) ? $filter->options['relationship'] : 'none';
-      $entityIds = $this->getEntityIds($relationship);
-      $values = $this->getReferencedValues($entityIds, $table, $column, $referenceColumn);
+      $values = $this->getFilterIds($relationship);
 
       // Set a default value.
       $min = 0;
